162d2bd0fd737887369dc4f86b0b6e3da9260bb0
Andreas Visagie <doctormit768@gmail.com>
2022-10-16 12:15:57 +0200

    Add Country

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,15 @@
-//
-// Created by jacom on 2022/10/15.
-//
+#include <iostream>
+#include "include/Country.h"
+#include "include/AbstractCountry.h"
+using namespace std;
+
+int main() {
+    AbstractCountry* x = new Country("jack");
+
+    AbstractCountry* y = x->cloneCountry(); 
+
+    cout << x->getName() << endl;   
+    delete y;
+    cout << "hello world" << endl;
+    return 0;
+}
